6a47d5f18f2af961e4cbf642216ce730ea37c0fe
Predrag Knezevic <pedjak@gmail.com>
2018-03-01 18:13:09 +0100

    fix: external config files will not be additionally auto-configured.
    The external config files will be properly detected in all cases:
    * mounted from outside using `-v /foo/conf:/config`
    * mounted directly using `-v /foo/mycassandra.yaml:/opt/dse/resources/cassandra/conf/cassandra.yaml`
    * mounted in a directory using `-v /foo/mycassandraConf:/opt/dse/resources/cassandra/conf`
    
    Finally, auto-configuring can be fully disabled by providing any value to `DSE_AUTO_CONF_OFF` env varibale

--- base/files/base-checks.sh
+++ base/files/base-checks.sh
@@ -3,6 +3,8 @@
 # if their counterparts exist in /config folder
 ############################################
 
+declare -A linkedExternalConf
+
 function link_external_config {
     for conf_file in $(cat /overwritable-conf-files) ; do
         ext_conf_file="/config/$(basename "$conf_file")"
@@ -11,8 +13,33 @@ function link_external_config {
             echo "Linking $embedded_conf_file to $ext_conf_file"
             mkdir -p "$(dirname "$embedded_conf_file")"
             ln -sf "$ext_conf_file" "$embedded_conf_file"
+            linkedExternalConf["$embedded_conf_file"]="$ext_conf_file"
         fi
     done
+}
+
+function is_external_file {
+
+    MP="$(df --output=target "$1" | tail -n +2)"
+    if [ "$MP" != "/" ] && grep -q "^Users $MP" /proc/mounts ; then
+        echo "$1 mount point is $MP"
+        true
+    else
+        false
+    fi
+}
+
+function should_auto_configure {
+
+    if [ ! -z "$DSE_AUTO_CONF_OFF" ]; then
+        false
+    else
+        if is_external_file "$1" ; then
+            false
+        else
+            true
+        fi
+    fi
 }
 
 ############################################
